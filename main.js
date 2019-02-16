50df2f09c52fbcf82c46500f424d1ca5f48936d3
Asdethprime <asdethprime@gmail.com>
2019-02-16 17:57:24 +1100

    added basic routes

--- main.js
+++ main.js
@@ -3,6 +3,31 @@ var express = require('express');
 var app = express();
 app.set('port', process.env.PORT || 3000 );
 
+// home page 
+app.get('/', function(req, res){
+    res.type('text/plain');
+    res.send('Home Page');
+});
+
+// Doner page
+app.get('/doner', function(req, res){
+    res.type('text/page');
+    res.send('This is the page for blood doners');    
+});
+
+// Recipient page
+app.get('/recipient', function(req, res){
+    res.type('text/plain');
+    res.send('This is the recipient page');
+});
+
+// God page - just a shameless joke
+app.get('/asdeth', function(req, res){
+    res.type('text/plain');
+    res.send('My god has arrived');
+    res.send(' I bow before your granduer');
+})
+
 // Custom 404 page
 app.use(function (req, res){
     res.type('text/plain');
