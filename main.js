a35f895a0821c45b455a9f9d7c67054b19452a9b
Asdethprime <asdethprime@gmail.com>
2019-02-16 17:36:38 +1100

    First commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// Basic setup
+var express = require('express');
+var app = express();
+app.set('port', process.env.PORT || 3000 );
+
+// Custom 404 page
+app.use(function (req, res){
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+
+// Custom 500 page
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server Error');
+});
+
+// Bootstrapping the app
+app.listen(app.get('port'), function() {
+    console.clear();
+    console.log('______')
+    console.log('Server');
+    console.log('______');
+    console.log('');
+    console.log('Bro I am listening on http://localhost:' + app.get('port'));
+    console.log('You can kill me by pressing Ctrl + C');
+});
